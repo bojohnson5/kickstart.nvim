330272ffe946bd5c16fc90178a1cb2d4cf03e62e
Johnson, Bo <johnsonba@ornl.gov>
2024-11-26 15:44:06 -0500

    clean up

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -1,18 +1,4 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
 return {
-  -- 'windwp/nvim-autopairs',
-  -- event = 'InsertEnter',
-  -- -- Optional dependency
-  -- dependencies = { 'hrsh7th/nvim-cmp' },
-  -- config = function()
-  --   require('nvim-autopairs').setup {}
-  --   -- If you want to automatically add `(` after selecting a function or method
-  --   -- local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-  --   -- local cmp = require 'cmp'
-  --   -- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  -- end,
   'echasnovski/mini.pairs',
   event = 'VeryLazy',
   opts = {
@@ -30,7 +16,4 @@ return {
       ['$'] = { action = 'closeopen', pair = '$$' },
     },
   },
-  config = function(_, opts)
-    require('mini.pairs').setup(opts)
-  end,
 }
